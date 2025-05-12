fd620d13961b77155d9cac894c12f0f636142c0e
Andrey P <petr.and.vic@gmail.com>
2025-05-12 14:32:48 +0400

    Создал классы сущностей и класс контекста

---
+++ ShopData/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopData.Models;
+
+public partial class Customer
+{
+    [Key]
+    [Column("CustomerID")]
+    public int CustomerId { get; set; }
+
+    [StringLength(50)]
+    public string FirstName { get; set; } = null!;
+
+    [StringLength(50)]
+    public string? Email { get; set; }
+
+    [StringLength(20)]
+    public string? Phone { get; set; }
+
+    [StringLength(300)]
+    public string? CustomerAddress { get; set; }
+
+    [InverseProperty("Customer")]
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
