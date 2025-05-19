f8e0c0ec011e580ce9d1af27aaaa38bc7aa132df
Andrey P <petr.and.vic@gmail.com>
2025-05-19 18:09:25 +0400

    Добавил класс работы с данными

---
+++ ShopData/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace shopData.Models;
+
+public partial class Category
+{
+    [Key]
+    [Column("CategoryID")]
+    public int CategoryId { get; set; }
+
+    [StringLength(30)]
+    public string NameCategories { get; set; } = null!;
+
+    [InverseProperty("Category")]
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
