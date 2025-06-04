f678ffc3bf424d7bb8c7460080f72ce6422aeafe
Andrey P <petr.and.vic@gmail.com>
2025-06-04 20:20:25 +0400

    Сам проект

---
+++ adminShop/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using shopData.Models;
+
+namespace adminShop.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetCategories();
+        Task AddCategory(Category category);
+        Task DeleteCategory(int id);
+        Task<Category?> GetCategoryById(int id);
+        Task UpdateCategory(Category category);
+    }
+}
